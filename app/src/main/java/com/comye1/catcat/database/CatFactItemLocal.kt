5d8c2ddb76d05127dfd2ef93bee50bbe616db85f
comye1 <comye1@seoultech.ac.kr>
2021-12-25 01:06:16 +0900

    로컬 클래스 빠트림

--- app/src/main/java/com/comye1/catcat/database/CatFactItemLocal.kt
+++ app/src/main/java/com/comye1/catcat/database/CatFactItemLocal.kt
@@ -5,7 +5,6 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "catfact_table")
 data class CatFactItemLocal (
-    val created: Long = System.currentTimeMillis(),
     @PrimaryKey val id: String,
     val text: String
 )
