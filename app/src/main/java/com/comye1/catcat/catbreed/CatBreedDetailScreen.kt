2248aff4d3b7e2633f1d4da0812ac6c67545a023
comye1 <comye1@seoultech.ac.kr>
2022-01-02 21:34:52 +0900

    cat breed detail screen 작성중

--- app/src/main/java/com/comye1/catcat/catbreed/CatBreedDetailScreen.kt
+++ app/src/main/java/com/comye1/catcat/catbreed/CatBreedDetailScreen.kt
@@ -1,11 +1,54 @@
 package com.comye1.catcat.catbreed
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CatBreedDetailScreen(viewModel: CatBreedViewModel, id: String?, navigateBack: () -> Boolean) {
-    id?.let {
-        Text(text = viewModel.getCatBreedItem(id).toString())
+
+    val catBreedItem = if (id != null) viewModel.getCatBreedItem(id) else null
+
+    if (catBreedItem == null) {
+        CatBreedDetailScaffold(title = "Not Found", navigateBack = { navigateBack() }) {
+
+        }
+    } else {
+        CatBreedDetailScaffold(title = catBreedItem.name!!, navigateBack = { navigateBack() }) {
+            Text(text = catBreedItem.description!!)
+        }
+
+    }
+}
+
+@Composable
+fun CatBreedDetailScaffold(
+    title: String,
+    navigateBack: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = title) },
+                navigationIcon = {
+                    IconButton(onClick = { navigateBack() }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "navigate back"
+                        )
+                    }
+                },
+            )
+        }
+    ) {
+        Column(Modifier.padding(16.dp)) {
+            content()
+        }
     }
 }
