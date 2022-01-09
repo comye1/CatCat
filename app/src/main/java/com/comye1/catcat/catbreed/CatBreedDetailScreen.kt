565b922cc3bbd2439d5e93723a10fc0a05b905d5
comye1 <comye1@seoultech.ac.kr>
2022-01-09 14:49:06 +0900

    CatBreedDetail 레이아웃 수정

--- app/src/main/java/com/comye1/catcat/catbreed/CatBreedDetailScreen.kt
+++ app/src/main/java/com/comye1/catcat/catbreed/CatBreedDetailScreen.kt
@@ -2,14 +2,17 @@ package com.comye1.catcat.catbreed
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
+import com.comye1.catcat.catfact.LoadingAnimation
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.HorizontalPagerIndicator
@@ -30,32 +33,54 @@ fun CatBreedDetailScreen(viewModel: CatBreedViewModel, id: String, navigateBack:
         id에 해당하는 Breed 이미지
          */
         val catImages = viewModel.catImages.observeAsState()
-//        viewModel.getCatImagesById(id)
         CatBreedDetailScaffold(title = catBreedItem.name!!, navigateBack = { navigateBack() }) {
-            Text(text = catBreedItem.description!!)
             /*
             이미지 pager에 출력
              */
 //            catImages.value?.forEach {
 //                Text(text = it.url)
 //            }
-            catImages.value?.let {
-                val pagerState = rememberPagerState()
-                HorizontalPager(
-                    count = it.size, state = pagerState, modifier = Modifier.fillMaxHeight(.8f)
-                ) { page ->
-                    Image(
-                        painter = rememberImagePainter(
-                            data = it[page].url
-                        ),
-                        contentDescription = "cat image",
-                    )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(.5f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                if (catImages.value == null) {
+                    LoadingAnimation()
                 }
-                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                catImages.value?.let {
+                    val pagerState = rememberPagerState()
+                    HorizontalPager(
+                        count = it.size,
+                        state = pagerState,
+                        modifier = Modifier.fillMaxHeight(.8f)
+                    ) { page ->
+                        Image(
+                            painter = rememberImagePainter(
+                                data = it[page].url
+                            ),
+                            contentDescription = "cat image",
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
                     HorizontalPagerIndicator(
                         pagerState = pagerState,
                         activeColor = MaterialTheme.colors.primaryVariant,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
                     )
+                }
+            }
+            LazyColumn {
+                item {
+                    Text(text = catBreedItem.description!!)
+                }
+                item {
+                    Text(text = catBreedItem.description!!)
+                }
+                item {
+                    Text(text = catBreedItem.description!!)
                 }
             }
         }
